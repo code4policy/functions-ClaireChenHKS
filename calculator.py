cd0105e7f6de80cd5ebd9dd9652186dec123131b
ClaireChenHKS <liyangchen@hks.harvard.edu>
2026-01-18 14:45:48 -0500

    Addfunctions

--- calculator.py
+++ calculator.py
@@ -10,6 +10,20 @@ def subtract(a,b):
 def divide(a,b):
 	return a/b
 
+def square(a):
+    return a * a
+
+def cube(a):
+    return a ** 3
+
+def square_n_times(a, n):
+      result = a
+      total = 0
+      for _ in range(n):
+          result = square(result)
+          total += result
+      return total
+      
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
